bd0d9bca070df37677d689f6d1d3effad85ce750
yokomizomizuki <yokomizorpc@gmail.com>
2025-10-14 13:51:07 +0900

    実技カリキュラム2-2レビュー修正分_2回目

--- src/main/java/com/example/Example.java
+++ src/main/java/com/example/Example.java
@@ -7,8 +7,12 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,14 +29,14 @@ public class Example {
 	String password = "Yokomirpc";
 
 	@GetMapping("/api/users") // 呼び出されるメソッドを宣言
-	public List<String> getUsers(@RequestParam("userMatch") Integer value,
+	public ResponseEntity<List<Map<String, Object>>> getUsers(@RequestParam("userMatch") Integer value,
 			@RequestParam(name = "userKeyword", required = false) String keyword) throws SQLException {
-		List<String> userIds = new ArrayList<>();
-		List<String> userNames = new ArrayList<>();
+
+		List<Map<String, Object>> usersList = new ArrayList<Map<String, Object>>();
 
 		// 実行SQL
 		StringBuilder sb = new StringBuilder();
-		sb.append("SELECT *").append("FROM users ");
+		sb.append("SELECT * FROM users ");
 
 		boolean useParameter = false;
 		if (value == 0) {
@@ -58,26 +62,27 @@ public class Example {
 
 			try (ResultSet resultSet = statement.executeQuery()) {// 戻り値はResultSetで取得しレコード格納
 				while (resultSet.next()) { // nextメソッドでレコードを一行ずつ呼び込む
-					userIds.add(resultSet.getString("id")); // カラム名、idの値を取得
-					userNames.add(resultSet.getString("name")); // カラム名、nameの値を取得
+					Map<String, Object> users = new HashMap<String, Object>();
+					users.put("id", resultSet.getInt("id")); // カラム名、idの値を取得
+					users.put("name", resultSet.getString("name"));// カラム名、nameの値を取得
+					// MapをListに格納
+					usersList.add(new HashMap<>(users));
 				}
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
-			List<String> result = new ArrayList<>();
-			result.addAll(userIds);
-			result.addAll(userNames);
-			return result;
+
+			return ResponseEntity.ok(usersList);
 		}
 	}
 
 	@GetMapping("/api/skills") // 呼び出されるメソッドを宣言
-	public List<String> getSkills(@RequestParam("skillMatch") Integer value,
+	public ResponseEntity<List<Map<String, Object>>> getSkills(@RequestParam("skillMatch") Integer value,
 			@RequestParam(name = "skillKeyword", required = false) String keyword,
 			@RequestParam(name = "sortBy", required = false) String sortBy,
 			@RequestParam(name = "sortOrder", required = false) String sortOrder) throws SQLException {
-		List<String> names = new ArrayList<>();
-		List<String> skills = new ArrayList<>();
+
+		List<Map<String, Object>> skillsList = new ArrayList<Map<String, Object>>();
 
 		if (!"sName".equals(sortBy) && !"sSkill".equals(sortBy)) {
 			sortBy = "sName";
@@ -88,8 +93,7 @@ public class Example {
 
 		// 実行SQL
 		StringBuilder sb = new StringBuilder();
-		sb.append("SELECT users.name, skills.skill ").append("FROM users ")
-				.append("JOIN skills ON users.id = skills.user_id ");
+		sb.append("SELECT users.name, skills.skill ,skills.id FROM users JOIN skills ON users.id = skills.user_id ");
 
 		boolean useParameter = false;
 		if (value == 0) {
@@ -127,113 +131,128 @@ public class Example {
 
 			try (ResultSet rs = statement.executeQuery()) {// 戻り値はResultSetで取得しレコード格納
 				while (rs.next()) { // nextメソッドでレコードを一行ずつ呼び込む
-					names.add(rs.getString("name")); // カラム名、nameの値を取得
-					skills.add(rs.getString("skill")); // カラム名、skillの値を取得
+					Map<String, Object> skills = new HashMap<String, Object>();
+					skills.put("name", rs.getString("name")); // カラム名、nameの値を取得
+					skills.put("skill", rs.getString("skill")); // カラム名、skillの値を取得
+					skills.put("id", rs.getInt("id"));
+					skillsList.add(new HashMap<>(skills));
 				}
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
 
-			List<String> result = new ArrayList<>();
-			result.addAll(names);
-			result.addAll(skills);
-			return result;
+			return ResponseEntity.ok(skillsList);
 		}
 	}
 
-	@DeleteMapping({ "/api/users/{id}", "/api/users/{name}/skills/{skill}" })
-	public String delete(@PathVariable(value = "id", required = false) Integer id,
-			@PathVariable(value = "name", required = false) String name,
-			@PathVariable(value = "skill", required = false) String skill) throws FileNotFoundException {
+	@DeleteMapping("/api/users/{id}")
+	public ResponseEntity<Object> user_delete(@PathVariable(name = "id", required = false) Integer id)
+			throws FileNotFoundException {
 
 		// try〜catch〜resources構文を使用
 		try (Connection conn = DriverManager.getConnection(url, username, password)) { // JDBC: DBアクセスするためのAPI
 			conn.setAutoCommit(false);// autoCommit を無効にする
 
-			//ユーザーテーブル
-			if (id != null && (name == null && skill == null)) {
-				String countsql = "SELECT COUNT(*) FROM skills WHERE user_id = ?";
-				try (PreparedStatement stmt = conn.prepareStatement(countsql)) {
-					stmt.setInt(1, id);
-					try (ResultSet rs = stmt.executeQuery()) {
-						if (rs.next()) {
-							int count = rs.getInt(1);
-							if (count > 0) {
-								// ロールバックで変更を無効
-								conn.rollback();
-								return "そのユーザーはスキルテーブルに存在します";
-							}
+			// ユーザーテーブル
+			String countsql = "SELECT COUNT(*) FROM skills WHERE user_id = ?";
+			try (PreparedStatement stmt = conn.prepareStatement(countsql)) {
+				stmt.setInt(1, id);
+				try (ResultSet rs = stmt.executeQuery()) {
+					if (rs.next()) {
+						int count = rs.getInt(1);
+						if (count > 0) {
+							// ロールバックで変更を無効
+							conn.rollback();
+							return ResponseEntity.status(HttpStatus.CONFLICT).body(409);
 						}
 					}
 				}
-
-				String userDelete_sql = "DELETE FROM users WHERE id = ?";
-				try (PreparedStatement stmt = conn.prepareStatement(userDelete_sql)) {
-					stmt.setInt(1, id);
-					stmt.executeUpdate(); //実行
-				}
-				conn.commit(); // トランザクションを完了させSQL操作を保存
 			}
-			
-			//スキルテーブル
-			if (name != null && skill != null && (id == null)) {
-				String skillDelete_sql = "DELETE FROM skills USING users WHERE skills.user_id = users.id AND users.name = ? AND skills.skill = ?"; // 実行SQL
-				try (PreparedStatement stmt = conn.prepareStatement(skillDelete_sql)) {
-					stmt.setString(1, name);
-					stmt.setString(2, skill);
-					stmt.executeUpdate(); //実行
-				}
+
+			String userDelete_sql = "DELETE FROM users WHERE id = ?";
+			try (PreparedStatement stmt = conn.prepareStatement(userDelete_sql)) {
+				stmt.setInt(1, id);
+				stmt.executeUpdate(); // 実行
 				conn.commit(); // トランザクションを完了させSQL操作を保存
 			}
 
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		return "削除";
+
+		return ResponseEntity.status(HttpStatus.OK).body(200);
 	}
 
-	// フォーム送信処理
-	@PostMapping({ "/usersubmit", "/skillsubmit" })
-	public String Form(@RequestParam(required = false) String userName, @RequestParam(required = false) Long userId,
-			@RequestParam(required = false) String skill) {
+	@DeleteMapping("/api/skills/{id}")
+	public ResponseEntity<Object> skill_delete(@PathVariable(name = "id", required = false) Integer id)
+			throws FileNotFoundException {
+
 		try (Connection conn = DriverManager.getConnection(url, username, password)) { // JDBC: DBアクセスするためのAPI
 			conn.setAutoCommit(false);// autoCommit を無効にする
 
-			//ユーザーテーブル
-			if (userName != null) {
-				String userSql = "INSERT INTO users (name) VALUES (?)"; // 実行SQL
-				try (PreparedStatement stmt = conn.prepareStatement(userSql)) {
-					stmt.setString(1, userName); // SELECTクエリに入力した値をセット
-					stmt.executeUpdate(); //実行
-				}
+			// スキルテーブル
+			String skillDelete_sql = "DELETE FROM skills USING users WHERE skills.user_id = users.id AND skills.id = ?"; // 実行SQL
+			try (PreparedStatement stmt = conn.prepareStatement(skillDelete_sql)) {
+				stmt.setInt(1, id);
+				stmt.executeUpdate(); // 実行
 				conn.commit(); // トランザクションを完了させSQL操作を保存
 			}
 
-			//スキルテーブル
-			if (userId != null && skill != null) {
-				String getId = "SELECT 1 FROM users WHERE id = ?"; // 実行SQL
-				try (PreparedStatement stmt = conn.prepareStatement(getId)) {
-					stmt.setLong(1, userId); // SELECTクエリに入力した値をセット
-					try (ResultSet rs = stmt.executeQuery()) {
-						if (!rs.next()) {
-							conn.rollback();
-							return "ユーザーIDが存在しません";
-						}
-					}
-				}
-				String skillSql = "INSERT INTO skills (user_id, skill) VALUES (?, ?)"; // 実行SQL
-				try (PreparedStatement stmt = conn.prepareStatement(skillSql)) {
-					stmt.setLong(1, userId);
-					stmt.setString(2, skill);
-					stmt.executeUpdate(); //実行
-				}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return ResponseEntity.status(HttpStatus.OK).body(200);
+	}
+
+	// フォーム送信処理
+	@PostMapping("/usersubmit")
+	public ResponseEntity<Object> usersForm(@RequestParam(required = false) String userName) {
+		try (Connection conn = DriverManager.getConnection(url, username, password)) { // JDBC: DBアクセスするためのAPI
+			conn.setAutoCommit(false);// autoCommit を無効にする
+
+			// ユーザーテーブル
+			String userSql = "INSERT INTO users (name) VALUES (?)"; // 実行SQL
+			try (PreparedStatement stmt = conn.prepareStatement(userSql)) {
+				stmt.setString(1, userName); // SELECTクエリに入力した値をセット
+				stmt.executeUpdate(); // 実行
 				conn.commit(); // トランザクションを完了させSQL操作を保存
 			}
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return "登録";
+		return ResponseEntity.status(HttpStatus.OK).body(200);
+	}
 
+	@PostMapping("/skillsubmit")
+	public ResponseEntity<Object> skillsForm(@RequestParam(name = "userId", required = false) Integer userId,
+			@RequestParam(name = "skill", required = false) String skill) {
+		try (Connection conn = DriverManager.getConnection(url, username, password)) { // JDBC: DBアクセスするためのAPI
+			conn.setAutoCommit(false);// autoCommit を無効にする
+
+			// スキルテーブル
+			String getId = "SELECT 1 FROM users WHERE id = ?"; // 実行SQL
+			try (PreparedStatement stmt = conn.prepareStatement(getId)) {
+				stmt.setInt(1, userId); // SELECTクエリに入力した値をセット
+				try (ResultSet rs = stmt.executeQuery()) {
+					if (!rs.next()) {
+						conn.rollback();
+						return ResponseEntity.status(HttpStatus.NOT_FOUND).body(404);
+					}
+				}
+			}
+
+			String skillSql = "INSERT INTO skills (user_id, skill) VALUES (?, ?)"; // 実行SQL
+			try (PreparedStatement stmt = conn.prepareStatement(skillSql)) {
+				stmt.setInt(1, userId);
+				stmt.setString(2, skill);
+				stmt.executeUpdate(); // 実行
+				conn.commit(); // トランザクションを完了させSQL操作を保存
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return ResponseEntity.status(HttpStatus.OK).body(200);
 	}
 }
